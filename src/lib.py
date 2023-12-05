ae77e2b02e12be25a34e989f55d0bae99daf49f8
Gregg Eschelmuller <g.eschelmuller@gmail.com>
2023-12-05 09:38:33 -0800

    update saving functions

--- src/lib.py
+++ src/lib.py
@@ -8,6 +8,47 @@ import pandas as pd
 import nidaqmx
 
 
+def configure_input(fs):
+    input_task = nidaqmx.Task()
+    input_task.ai_channels.add_ai_voltage_chan("Dev1/ai0", min_val=0, max_val=5)
+    input_task.ai_channels.add_ai_voltage_chan("Dev1/ai1", min_val=0, max_val=5)
+    input_task.timing.cfg_samp_clk_timing(
+        fs, sample_mode=nidaqmx.constants.AcquisitionType.CONTINUOUS
+    )
+
+def configure_output():
+    # Outputs - have to create separate tasks for input/output
+    output_task = nidaqmx.Task()
+    output_task.do_channels.add_do_chan("Dev1/port0/line0") # Extensor
+    output_task.do_channels.add_do_chan("Dev1/port0/line1") # Flexor
+
+def generate_trial_dict():
+    template = {
+        "trial_num": [],
+        "move_times": [],
+        "wrist_x_end": [],
+        "wrist_y_end": [],
+        "curs_x_end": [],
+        "curs_y_end": [],
+        "end_angles": [],
+        "block": [],
+        "target_angle": [],
+        'rt': [],
+    }
+    return template
+
+def generate_position_dict():
+    template = {
+        "x_volts": [],
+        "y_volts": [],
+        "wrist_x": [],
+        "wrist_y": [],
+        "curs_x": [],
+        "curs_y": [],
+        "time": [],
+    }
+    return template
+
 # 24 inch diag - resololution 1920x1080
 def cm_to_pixel(cm):
     return cm * 36.14
@@ -16,6 +57,13 @@ def cm_to_pixel(cm):
 def pixel_to_cm(pix):
     return pix / 36.24
 
+def x_volt_to_pixel(volt):
+    pix = (volt - 2.2) * 550
+    return pix
+
+def y_volt_to_pixel(volt):
+    pix = (volt - 2.2) * 550
+    return pix
 
 def read_trial_data(file_name, sheet=0):
     # Reads in the trial data from the excel file
@@ -31,7 +79,6 @@ def exp_filt(pos0, pos1, alpha=0.5):
     y = (pos0[1] * alpha) + (pos1[1] * (1 - alpha))
     return [x, y]
 
-
 def get_xy(task):
     while True:
         vals = task.read(
@@ -42,23 +89,39 @@ def get_xy(task):
         elif not vals == None:
             x_data = vals[0]
             y_data = vals[1]
+            return [x_data[-1], y_data[-1]]
 
-            # If buffer contains multiple data points take the lastest one
-            if len(x_data) > 1:
-                x_data = [x_data[-1]]
-            if len(y_data) > 1:
-                y_data = [y_data[-1]]
 
-            # I don't remember why this check is here, but it doesn't work without it
-            if not len(vals[0]) == 0:
-                # Offset cursor to middle position
-                x = x_data[0] - 2.2
-                y = y_data[0] - 2.2
 
-                # Cursor gain
-                x *= 550
-                y *= 550
-                return [x, y]
+
+# Turned into two separate functions
+# def get_xy(task):
+#     while True:
+#         vals = task.read(
+#             number_of_samples_per_channel=nidaqmx.constants.READ_ALL_AVAILABLE
+#         )
+#         if vals == None:
+#             continue
+#         elif not vals == None:
+#             x_data = vals[0]
+#             y_data = vals[1]
+
+#             # If buffer contains multiple data points take the lastest one
+#             if len(x_data) > 1:
+#                 x_data = [x_data[-1]]
+#             if len(y_data) > 1:
+#                 y_data = [y_data[-1]]
+
+#             # I don't remember why this check is here, but it doesn't work without it
+#             if not len(vals[0]) == 0:
+#                 # Offset cursor to middle position
+#                 x = x_data[0] - 2.2
+#                 y = y_data[0] - 2.2
+
+#                 # Cursor gain
+#                 x *= 550
+#                 y *= 550
+#                return [x, y]
 
 
 def contains(small_circ, large_circ):
